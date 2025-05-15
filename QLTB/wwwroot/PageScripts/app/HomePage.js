6b80c172f6f12d33b35a931a5fe4e8ca3394f56f
zoee711 <ductranvan711@gmail.com>
2025-05-15 19:53:53 +0700

    xây dựng trang chủ: Banner

---
+++ QLTB/wwwroot/PageScripts/app/HomePage.js
@@ -0,0 +1,37 @@
+﻿const baseUrl = getRootLink();
+
+$(document).ready(function() {
+    // tạo banner
+    function initializeBanners() {
+        const banners = [
+            {
+                imageUrl: "https://huecit.vn/Portals/3/QuangCao/Nam2022/T10/UngDungCDSDN.png",
+            },
+            {
+                imageUrl: "https://huecit.vn/Portals/3/QuangCao/Nam2022/T4/BannerOpenData.jpg",
+            },
+            {
+                imageUrl: "https://huecit.vn/Portals/3/QuangCao/Nam2015/T11/HueCIT_Banner1.jpg",
+            }
+        ];
+
+        // tạo html
+        const carouselInner = $('.carousel-inner');
+        banners.forEach((banner, index) => {
+            const slide = $('<div>', {
+                class: `carousel-item ${index === 0 ? 'active' : ''} h-100`,
+                html: `<img src="${banner.imageUrl}" class="d-block w-100 h-100 img-fluid" alt="Banner ${index + 1}">`
+            });
+            carouselInner.append(slide);
+        });
+
+        // tạo carousel
+        $('#bannerCarousel').carousel({
+            interval: 5000,
+            wrap: true
+        });
+    }
+
+    // tạo banner khi load
+    initializeBanners();
+});
